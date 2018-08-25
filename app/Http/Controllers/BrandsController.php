4a676bb7b54b36c0bc2e131ec69dafe32bfc969c
MING MANEERAT <ming.maneerat@allianz.com>
2018-08-25 23:09:58 +0700

    Add Brand module --list and create

---
+++ app/Http/Controllers/BrandsController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Brand;
+use App\Http\Requests\BrandPost;
+use Illuminate\Http\Request;
+
+class BrandsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('brands.index', ['brands' => Brand::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(BrandPost $request)
+    {
+        //
+		$brand = Brand::create($request->all());
+		$brand->save();
+		
+		return view('brands.index', ['brands' => Brand::all()]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Brand $brand)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Brand  $brand
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Brand $brand)
+    {
+        //
+    }
+}
