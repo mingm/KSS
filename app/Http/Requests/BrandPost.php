d9c4029cd48d09977ac95d77e0bd076b53376e58
Ming Maneerat <ming.maneerat@allianz.com>
2018-09-09 18:30:09 +0700

    Update Brand module

--- app/Http/Requests/BrandPost.php
+++ app/Http/Requests/BrandPost.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Brand;
 use Illuminate\Foundation\Http\FormRequest;
 
 class BrandPost extends FormRequest
@@ -23,9 +24,36 @@ class BrandPost extends FormRequest
      */
     public function rules()
     {
-        return [
-            'name' => 'required|unique:brands|max:255',
-			'status' => 'required|in:Active,Inactive',
-        ];
+        //return [
+        //    'name' => 'required|unique:brands|max:255',
+		//	'status' => 'required|in:Active,Inactive',
+        //];
+
+		switch($this->method())
+		{
+			case 'GET':
+			case 'DELETE':
+			{
+				return [];
+			}
+			case 'POST':
+			{
+				return [
+					'name' => 'required|unique:brands|max:255',
+					'status' => 'required|in:Active,Inactive',
+				];
+			}
+			case 'PUT':
+			case 'PATCH':
+			{
+				$brand = Brand::find($this->brand->id);
+				return [
+					'name' => 'required|max:255|unique:brands,name,'.$brand->id,
+					'status' => 'required|in:Active,Inactive',
+				];
+			}
+			default:break;
+		}
+		
     }
 }
--- resources/views/brands/index.blade.php
+++ resources/views/brands/index.blade.php
@@ -1,173 +1,83 @@
 @extends('layouts.kss')
 
 @section('content')
-<div class="container"><!-- CONTENT ทั้งหมด นอกนั้นเป็น Blade -->
 
-	<div class="row">
-		<div class="col-md-12">
-
-			@include('shared.breadcrumb', ['current' => 'แบรนด์'])
-
-			<div class="panel panel-default">
-				<div class="panel-heading">
-					<div class="page-heading"> <i class="glyphicon glyphicon-edit"></i> จัดการแบรนด์</div>
-				</div> <!-- /panel-heading -->
-				<div class="panel-body">
-
-					<div class="remove-messages"></div>
-
-					<div class="div-action pull pull-right" style="padding-bottom:20px;">
-						<button class="btn btn-default button1" data-toggle="modal" data-target="#addBrandModel"> <i class="glyphicon glyphicon-plus-sign"></i> เพิ่มรายการ </button>
-					</div> <!-- /div-action -->				
-					
-					<table class="table" id="manageBrandTable">
-						<thead>
-							<tr>							
-								<th>ชื่อแบรนด์</th>
-								<th>สถานะ</th>
-								<th style="width:15%;">คำสั่ง</th>
-							</tr>
-						</thead>
-						<tbody>
-						@foreach ($brands as $brand)
-							<tr>
-								<td>{{ $brand->name }}</td>
-								<td>
-									@if ($brand->status === 'Active')
-										เปิดการใช้งาน
-									@else
-										ยังไม่เปิดใช้งาน
-									@endif
-								</td>
-								<td>
-									<a type="button" data-toggle="modal" data-target="#editBrandModel" onclick="editBrands('{{ $brand->id }}')">
-									<i class="glyphicon glyphicon-edit"></i></a>
-									
-								</td>
-							</tr>
-						@endforeach
-						</tbody>
-					</table>
-					{{ $brands->links() }}
-					<!-- /table -->
-
-				</div> <!-- /panel-body -->
-			</div> <!-- /panel -->		
-		</div> <!-- /col-md-12 -->
-	</div> <!-- /row -->
-
-	<div class="modal fade" id="addBrandModel" tabindex="-1" role="dialog">
-	  <div class="modal-dialog">
-		<div class="modal-content">
+        <div id="page-wrapper">
+			<div class="row horizontal">
+				<div class="col-lg-12">
+					<h2 class="page-header">แบรนด์สินค้า</h2>
+				</div>
+            </div>
 			
-			<form class="form-horizontal" id="submitBrandForm" action="/api/brands" method="POST">
-				@csrf
-			  <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title"><i class="glyphicon glyphicon-plus-sign"></i> เพิ่มรายการ</h4>
-			  </div>
-			  <div class="modal-body">
-
-				<div id="add-brand-messages"></div>
-
-				<div class="form-group">
-					<label for="name" class="col-sm-3 control-label">ชื่อแบรนด์ : </label>
-					<label class="col-sm-1 control-label">: </label>
-						<div class="col-sm-8">
-							<input type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" id="name" placeholder="ชื่อแบรนด์" name="name" required autofocus>
+			<div class="row horizontal">
+				<div class="col-lg-12">
+					<div class="panel panel-info">
+						<div class="panel-heading">
+							<h4>แบรนด์สินค้า</h4>
 						</div>
-				</div> <!-- /form-group-->	         	        
-				<div class="form-group">
-					<label for="status" class="col-sm-3 control-label">สถานะ :  </label>
-					<label class="col-sm-1 control-label">: </label>
-						<div class="col-sm-8">
-							<select class="form-control{{ $errors->has('status') ? ' is-invalid' : '' }}" id="status" name="status">
-								<option value="">~~เลือกรายการ~~</option>
-								<option value="Active" selected>เปิดการใช้งาน</option>
-								<option value="Inactive">ยังไม่เปิดใช้งาน</option>
-							</select>
+							<div class="panel-body">
+							
+							<div class="panel-group">
+								<div class="col-md-8"></div>
+								<div class="input-group custom-search-form col-md-4">
+									<input type="text" class="form-control" placeholder="Search...">
+									<span class="input-group-btn">
+									<button class="btn btn-default" type="button">
+										<i class="fa fa-search"></i>
+									</button>
+								</span>
+								</div>
+							</div>	
+							
+							<div class="table-responsive">
+								<table class="table table-condensed table-hover">
+									<thead>
+										<tr>
+											<th class="text-center" style="width: 100px;">ลำดับ</th>
+											<th>รายการ</th>
+											<th class="text-center" style="width: 200px;">สถานะ</th>
+											<th class="text-center" style="width: 200px;">คำสั่ง</th>
+										</tr>
+									</thead>
+									<tbody>
+									@foreach ($brands as $brand)
+										<tr>
+											<td class="text-center">{{ $loop->iteration + (($brands->currentPage() - 1) * $brands->perPage()) }}</td>
+											<td>{{ $brand->name }}</td>
+											<td>
+											@if ($brand->status === 'Active')
+												<td class="text-center"><label class="label label-success">เปิดการใช้งาน</label></td>
+											@else
+												<td class="text-center"><label class="label label-danger">ยังไม่เปิดใช้งาน</label></td>
+											@endif
+											</td>											
+											<td class="text-center">
+												<div class="btn-group">
+													<a href="{{ url('/brands/' . $brand->id ) }}" class="btn btn-xs btn-warning" data-toggle="tooltip" title="Edit"><i class="glyphicon glyphicon-log-in"></i></a>
+													<!--button class="btn btn-xs btn-danger" data-toggle="tooltip" title="Remove"><i class="glyphicon glyphicon-trash"> </i></button-->
+												</div>
+											</td>
+										</tr>
+									@endforeach
+									</tbody>
+								</table>
+							</div>
 						</div>
-				</div> <!-- /form-group-->	         	        
-
-			  </div> <!-- /modal-body -->
-			  
-			  <div class="modal-footer">
-				<button type="button" class="btn btn-default" data-dismiss="modal">ปิด</button>
-				
-				<button type="submit" class="btn btn-primary" id="createBrandBtn" data-loading-text="Loading..." autocomplete="off">บันทึกรายการ</button>
-			  </div>
-			  <!-- /modal-footer -->
-			</form>
-			 <!-- /.form -->
-		</div>
-		<!-- /modal-content -->
-	  </div>
-	  <!-- /modal-dailog -->
-	</div>
-	<!-- / add modal -->
-
-	<!-- edit brand -->
-	<div class="modal fade" id="editBrandModel" tabindex="-1" role="dialog">
-	  <div class="modal-dialog">
-		<div class="modal-content">
+					</div>
+				</div>
+			</div>
 			
-			<form class="form-horizontal" id="editBrandForm" action="/api/brands" method="PUT">
-			@csrf
-			  <div class="modal-header">
-				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-				<h4 class="modal-title"><i class="fa fa-edit"></i> แก้ไขแบรนด์สินค้า</h4>
-			  </div>
-			  <div class="modal-body">
-
-				<div id="edit-brand-messages"></div>
-
-				<div class="modal-loading div-hide" style="width:50px; margin:auto;padding-top:50px; padding-bottom:50px;">
-							<i class="fa fa-spinner fa-pulse fa-3x fa-fw"></i>
-							<span class="sr-only">Loading...</span>
-						</div>
-
-				  <div class="edit-brand-result">
-					<div class="form-group">
-						<label for="name" class="col-sm-3 control-label">ชื่อแบรนด์ : </label>
-						<label class="col-sm-1 control-label">: </label>
-							<div class="col-sm-8">
-							  <input type="text" class="form-control" id="name" placeholder="ชื่อแบรนด์" name="name" autocomplete="off">
-							</div>
-					</div> <!-- /form-group-->	         	        
-					<div class="form-group">
-						<label for="status" class="col-sm-3 control-label">สถานะ :  </label>
-						<label class="col-sm-1 control-label">: </label>
-							<div class="col-sm-8">
-							  <select class="form-control" id="status" name="status">
-							<option value="">~~เลือกรายการ~~</option>
-							<option value="Active" selected>เปิดการใช้งาน</option>
-							<option value="Inactive">ยังไม่เปิดใช้งาน</option>
-							  </select>
-							</div>
-					</div> <!-- /form-group-->	
-				  </div>         	        
-				  <!-- /edit brand result -->
-
-			  </div> <!-- /modal-body -->
-			  
-			  <div class="modal-footer editBrandFooter">
-				<button type="button" class="btn btn-default" data-dismiss="modal"> <i class="glyphicon glyphicon-remove-sign"></i> ปิด</button>
-				
-				<button type="submit" class="btn btn-success" id="editBrandBtn" data-loading-text="Loading..." autocomplete="off"> <i class="glyphicon glyphicon-ok-sign"></i> บันทึกรายการ</button>
-			  </div>
-			  <!-- /modal-footer -->
-			</form>
-			 <!-- /.form -->
-		</div>
-		<!-- /modal-content -->
-	  </div>
-	  <!-- /modal-dailog -->
-	</div>
-	<!-- / add modal -->
-	<!-- /edit brand -->
-
-
-	<script src="custom/js/brand.js"></script>	
+			<div class="row horizontal">
+				<div class="col-lg-12">
+					<ul class="pagination pull-left">
+						<li><a href="{{ url('/brands/create') }}" class="btn btn-primary"><i class="glyphicon glyphicon-plus-sign"></i> เพิ่มรายการ</a></li>
+					</ul>
+					{{ $brands->links('shared.pagination') }}
+				</div>
+			</div>
 	
-	</div> <!-- container --><!-- CONTENT ทั้งหมด นอกนั้นเป็น Blade -->	
+	<!--End Content-->
+
+	
+	</div>
 @endsection
