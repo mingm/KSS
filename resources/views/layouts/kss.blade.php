a4bb6e5161ac6152f279a40e4a5ae8c03b33fb2b
Ming Maneerat <ming.maneerat@allianz.com>
2018-10-15 06:42:30 +0700

    Update dashboard page

--- resources/views/layouts/kss.blade.php
+++ resources/views/layouts/kss.blade.php
@@ -64,7 +64,7 @@
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
                         <li>
-                            <a href="index.html"><i class="fa fa-dashboard fa-fw"></i> แผงควบคุม</a>
+                            <a href="{{ url('/') }}"><i class="fa fa-dashboard fa-fw"></i> แผงควบคุม</a>
                         </li>
                         <li>
                             <a href="{{ url('claims') }}"><i class="fa fa-plus-circle"></i> เพิ่มรายการเคลมสินค้า</a>
